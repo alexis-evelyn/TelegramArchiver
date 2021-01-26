b1e979f64d183153646efd198c3636a9f8fc8c37
Alexis Evelyn <alexis.a.evelyn@gmail.com>
2021-01-25 22:01:24 -0500

    Attempt At Robust Mass Downloader

--- main.py
+++ main.py
@@ -1,42 +1,77 @@
 #!/usr/bin/python3
 
-# Channels
-# The_DonaId, CoronaChanNews2, AltSkullMirror
-
-from telethon import TelegramClient, events, sync
-
-import pandas as pd
+import json
+import asyncio
 import configparser
 
-# Get API Login Auth
+from typing import Union, TextIO, List
+
+from telethon import TelegramClient, errors
+from telethon.client.messages import _IDsIter, _MessagesIter
+from telethon.tl.types import Chat, Message
+
 config = configparser.ConfigParser()
 config.read('login.ini')
 
 # API Login Auth
-api_id = config['auth']['id']
-api_hash = config['auth']['key']
-channel_name: str = config['archive']['channel']
+api_id: int = int(config['auth']['id'])
+api_hash: str = config['auth']['key']
+# channel_name: str = config['archive']['channel']
 
 client = TelegramClient('python-archiver', api_id, api_hash)
 client.start()
 
-# Messages
-messages = client.get_messages(channel_name, limit=int(config['archive']['limit']))
-# messages[0].download_media()
 
-print(f"{messages.total} Total Messages")
-
-# TODO: Verify How To Not Crash On Too Small Number Of Messages
-channel: pd.DataFrame = pd.DataFrame()
-for x in range(0, messages.total):
+async def download_channel(channel: str) -> dict:
     try:
-        message_dict: dict = messages[x].to_dict()
-        channel = channel.append(message_dict, ignore_index=True)
-    except:
-        print(f"Failed {x}/{messages.total}!!!")
+        async with client.takeout(contacts=True, users=True, chats=True, megagroups=True, channels=True, files=False) as takeout:
+            chat: Chat = await client.get_entity(channel)
+            messages: Union[_MessagesIter, _IDsIter] = takeout.iter_messages(chat, wait_time=0)
 
-# print(channel)
-print(channel.message)
-# print(f"{messages.total} Total Messages")
+            print(f"Downloading Channel: {channel}")
+            # print(f"{messages.total} Total Messages")
 
-channel.to_csv(f"{channel_name}.csv")
+            messages_dict: dict[Message] = {}
+            async for message in messages:
+                # print(message.id, message.text)
+                messages_dict[message.id] = message.to_dict()
+
+            return messages_dict
+    except errors.TakeoutInitDelayError as e:
+        print('Must wait ', e.seconds, ' before takeout')
+
+
+async def save_channel(channel: str):
+    print(f"Validating Channel: {channel}")
+    results: dict = await download_channel(channel)
+
+    print(f"Saving Channel: {channel}")
+    save: TextIO = open(file=f"working/messages/{channel}.json", mode="w")
+    save.writelines(json.dumps(obj=results, indent=4, sort_keys=True, default=str))
+    save.close()
+
+
+async def download_channels():
+    channel_list_path: str = "working/telegram.txt"
+    channel_list_file: TextIO = open(file=channel_list_path, mode="r")
+    channel_list: List[str] = channel_list_file.readlines()
+
+    for line in channel_list:
+        clean: str = line.lstrip("https://t.me/").strip()
+
+        if clean == "":
+            continue
+
+        try:
+            await save_channel(channel=clean)
+        except ValueError as e:
+            if "Cannot find any entity corresponding to" not in e.args[0]:
+                print(e)
+                break
+
+
+print("Starting Download!!!")
+loop = asyncio.get_event_loop()
+loop.run_until_complete(download_channels())
+# loop.run_until_complete(download_channel("magalife"))
+print("Finished!!!")
---
+++ old_main.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+# Channels
+# The_DonaId, CoronaChanNews2, AltSkullMirror
+
+from telethon import TelegramClient, events, sync
+
+import pandas as pd
+import configparser
+
+# Get API Login Auth
+config = configparser.ConfigParser()
+config.read('login.ini')
+
+# API Login Auth
+api_id = config['auth']['id']
+api_hash = config['auth']['key']
+channel_name: str = config['archive']['channel']
+
+client = TelegramClient('python-archiver', api_id, api_hash)
+client.start()
+
+# Messages
+messages = client.get_messages(channel_name, limit=int(config['archive']['limit']))
+# messages[0].download_media()
+
+print(f"{messages.total} Total Messages")
+
+# TODO: Verify How To Not Crash On Too Small Number Of Messages
+channel: pd.DataFrame = pd.DataFrame()
+for x in range(0, messages.total):
+    try:
+        message_dict: dict = messages[x].to_dict()
+        channel = channel.append(message_dict, ignore_index=True)
+    except:
+        print(f"Failed {x}/{messages.total}!!!")
+
+# print(channel)
+print(channel.message)
+# print(f"{messages.total} Total Messages")
+
+channel.to_csv(f"{channel_name}.csv")
